046850c913bcab5821eddfb5ad00e317661f4853
ElAmirr <othmaniamir41@gmail.com>
2021-09-17 14:38:15 +0200

    add registration futures

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '6b5d8b05c4cbc96a56c31053'
 db = SQLAlchemy(app)
-
-
 from market import routes
